5cde30b2176a78ef1a4db80b6ec064d68aae2932
josue santos <josue.santos.dev@gmail.com>
2024-08-27 11:41:56 -0300

    ports: create a port database using postgres

--- ports/database.go
+++ ports/database.go
@@ -1,1 +1,37 @@
 package ports
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type Database interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+type Postgres struct {
+	db *sql.DB
+}
+
+func PostgresConnection(dataSource string) (*Postgres, error) {
+	db, err := sql.Open("postgres", dataSource)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &Postgres{db: db}, nil
+}
+
+func (p *Postgres) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return p.db.Query(query, args...)
+}
+
+func (p *Postgres) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return p.db.Exec(query, args...)
+}
